9accd7f08c40ccf23d2abe97844eeb98634c3443
Stephanie Organista <55989682+StephanieOrganista@users.noreply.github.com>
2020-10-22 23:02:30 -0500

    Add files via upload
    Maze Game start

---
+++ mazeGame.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main {
+
+string userName;
+
+cout << "Welcome! Please enter your name:";
+cin >> userName; 
+
+cout << endl;
+
+cout << "Happy Halloween " << userName << "! You and your 4 friends decide to explore the mysterious haunted house 
+in your neighborhood."
+
+
+
+return 0;
+}
+
